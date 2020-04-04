24bbd508459567b6cb65360333079932b7a79954
raveen15 <55964271+raveen15@users.noreply.github.com>
2020-04-03 20:32:19 -0400

    Add files via upload

---
+++ constant.js
@@ -0,0 +1,111 @@
+//constant variables 
+var BOARD = {
+	WIDTH : 22,
+	HEIGHT : 6,
+	OWN_WIDTH : 22,
+	OWN_HEIGHT : 2,
+	GAME_BOARD_ID : "gameBoard",
+	OWN_BOARD_ID : "ownBoard",
+	PENALTY_ONE : 1,
+	PENALTY_THREE : 3,
+	MESSAGE_COLOR : "White",
+	TIMER_SEC : 60,
+	TIMER_LIMIT : 10,
+	TIMER_INTERVAL : 1000,
+	DIALOG_TIMEOUT : 3000,
+	CHAT_COLOR : ["Aqua", "Turquoise", "Yellow", "Lime", "Chartreuse", "Violet", "Gold", "Orange"],
+	USER_PREFIX : "USER_"
+}
+
+//For node.js console
+var CMD = {
+	CHAT : "::CHAT::",
+	JOIN : "::JOIN::",
+	SYNC : "::SYNC::",
+	START : "::START::", 
+	TURN : "::TURN::", 
+	EXIT : "::EXIT::",
+	INFO : "::INFO::",
+	PRIVATE_INFO : "::PRIVATE_INFO::",
+	PENALTY : "::PENALTY::",
+	ROLLBACK : "::ROLLBACK",
+	WIN : "::WIN::",
+	DISCONNECT : "DISCONNECT",
+	TIMER : "::TIMER::"
+};
+
+//For functions initiated from user
+var INLINE_CMD = {
+	"HELP" : "/?",
+	"CHANGE_NAME" : "/name",
+	"RESTART" : "/r"
+};
+
+//For messages 
+var MESSAGE = {
+	MSG_BTN_START : "Start",
+	MSG_BTN_NEXT_TURN : "End turn",
+	MSG_START : "The game has started.<br/>===============================<br/><br/>Welcome to Rummy-O. <br/>Press /? for instructions. <br/>To enter your username, enter command /name [username].<br/><br/>===============================<br/><br/>",
+	MSG_EXIT : "The game is over.",
+	MSG_TURN : "{0} You have ended your turn.",
+	MSG_NEXT_TURN : "{0}'s turn.",
+	MSG_JOIN : "{0} joined.",
+	MSG_DISCONNECT : "{0} disconnected.",
+	MSG_CLIENT_COUNT : "[{0} are online]",
+	MSG_TURN_COUNT : "{0} turn count",
+	MSG_GAME_READY : "Game is ready",
+	MSG_GAME_PLAYING : "The game is in progress",
+	MSG_MY_INFO : "Welcome {0}!",
+	MSG_WIN : "{0} won! Congratulations!.",
+	MSG_PENALTY : "{0} takes {1} penalty tiles.",
+	MSG_REGISTER : "(register)",
+	MSG_UNREGISTER : "(unregister)",
+	MSG_REMAIN_TILES : "{0} tiles remaining",
+	MSG_TIMER : "{0} seconds remain until end of turn.",
+	MSG_HELP : "<span style=\"color:Gold;\"><br />===============================<br/><br/>Rummy-O Instructions<br/><br/>There are many options to win the game. One way is to put all 3 or 4 of the same numbers with different colors, or 3 or more tiles of the same color and consecutive numbers. For the first move, you must make your first move with a group or run carrying at least 30 points in total. If it is less than 30, you have to draw a tile from the table and add it to the rack. If you don't play anything you will get 3 penalty tiles. After that you have to wait for your next move. <br/><br/>===============================<br/><br/></span>",
+	MSG_CHANGE_NAME : "{0} has been renamed to {1}.",
+	MSG_RESTART : "The game is restarted."
+};
+
+var UTIL = {
+
+	makeCommand: function(command, param) {
+        return { 
+            "command" : command, 
+            "param" : param
+        };
+    },
+
+	getMessage: function(msg, param0, param1) {
+
+		if(param0 != null) {
+			msg = msg.replace("{0}", param0);
+		}
+
+		if(param1 != null) {
+			msg = msg.replace("{1}", param1);
+		}
+
+		return msg;
+    },
+
+    printNowDate: function() {
+        now = new Date();
+        year = "" + now.getFullYear();
+        month = "" + (now.getMonth() + 1); if (month.length == 1) { month = "0" + month; }
+        day = "" + now.getDate(); if (day.length == 1) { day = "0" + day; }
+        hour = "" + now.getHours(); if (hour.length == 1) { hour = "0" + hour; }
+        minute = "" + now.getMinutes(); if (minute.length == 1) { minute = "0" + minute; }
+        second = "" + now.getSeconds(); if (second.length == 1) { second = "0" + second; }
+        return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+    },
+
+    random4digit: function() {
+    	return Math.floor(Math.random()*9000) + 1000;
+    },
+
+    randomChatColor: function() {
+    	return BOARD.CHAT_COLOR[Math.floor(Math.random() * BOARD.CHAT_COLOR.length)];
+    }
+
+}
---
+++ server.js
@@ -0,0 +1,391 @@
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
+
+//constant
+var fs = require('fs');
+eval(fs.readFileSync('./constant.js')+'');
+eval(fs.readFileSync('./rummikub.js')+'');
+
+//start server
+app.use(express.static(__dirname + "/"))
+
+var server = http.createServer(app)
+server.listen(port)
+console.log("http server listening on %d", port);
+
+var webSocketServer = new WebSocketServer({server: server})
+console.log("websocket server created");
+
+//static variable
+var rummikub = new Rummikub();
+var gamePlayingFlag = false;
+var turnCount = 1;
+var currentPlayer = {};
+var lastSyncTiles = [];
+
+var timer = null;
+var sec = 0;
+
+//broadcast client
+webSocketServer.broadcast = function(data) {
+    //console.log("[broadcast msg]=" + JSON.stringify(data));
+    for (var i in rummikub.users) {
+        rummikub.users[i].ownWebsocket.send(JSON.stringify(data));
+    }
+};
+
+//send specific client
+webSocketServer.sendMessage = function(data, id) {
+    //console.log("[send msg -> " + id + "]=" + JSON.stringify(data));
+    for (var i in rummikub.users) {
+        if(id == rummikub.users[i].id) {
+            rummikub.users[i].ownWebsocket.send(JSON.stringify(data));
+        }
+    }
+};
+
+//connect client
+webSocketServer.on("connection", function(ws) {
+
+    var user = new User(BOARD.USER_PREFIX + UTIL.random4digit(), ws, UTIL.randomChatColor());
+    rummikub.users.push(user);
+
+    processJoin(user);
+    
+    //receive message
+    ws.on('message', function(message) {
+
+        var requestObject = JSON.parse(message);
+
+        //console.log("[Message Received] Command : " + requestObject.command + " Param : " + requestObject.param);
+        console.log("[Message Received] Command : " + requestObject.command);
+        
+        if(requestObject.command == CMD.START) {
+
+            processStart(user);
+
+        }else if(requestObject.command == CMD.TURN) {
+
+            processTurn(requestObject.param, user);
+
+        }else if(requestObject.command == CMD.SYNC) {
+            
+            processSync(requestObject.param);
+
+        }else if(requestObject.command == CMD.CHAT) {
+
+            processChat(requestObject.param);
+
+        }else {
+            // nothing happen
+        }
+		
+    });
+    
+    ws.on("close", function() {
+        processDisconnect(user);
+    })
+
+    //Contains the about from the board
+    function boardInfo() {
+        return { 
+            "gamePlayingFlag" : gamePlayingFlag, 
+            "turnCount" : turnCount, 
+            "currentPlayerID" : currentPlayer.id,
+            "usersInfo" : usersInfo()
+        };
+    }
+
+    //Contains the information about the user
+    function userInfo(user) {
+        return {
+            "id" : user.id,
+            "registerYN" : user.registerYN,
+            "use" : user.use,
+            "own" : user.own
+        };
+    }
+
+    function usersInfo() {
+
+        var usersInfo = [];
+
+        for (var i in rummikub.users) {
+            usersInfo.push(userInfo(rummikub.users[i]));
+        }
+
+        return usersInfo;
+    }
+
+    //Contains the user info
+    function userScoreInfo() {
+
+        var userScoreInfo = [];
+        
+        var winner = {};
+        winner.isWin = true;
+        winner.id = "";
+        winner.score = 0;
+
+        for(var idx in rummikub.users) {
+            if(rummikub.users[idx].own.length == 0) {
+                winner.id = rummikub.users[idx].id;
+            }else {
+                var loser = {};
+                loser.isWin = false;
+                loser.id = rummikub.users[idx].id;
+                loser.score = 0;
+                for(var ownIdx in rummikub.users[idx].own) {
+                    loser.score -= Number(rummikub.users[idx].own[ownIdx].score);
+                    winner.score += Number(rummikub.users[idx].own[ownIdx].score);
+                }
+                userScoreInfo.push(loser);
+            }
+        }
+        userScoreInfo.push(winner);
+
+        return userScoreInfo;
+    }
+
+    //Processing function when users join
+    function processJoin(user) {
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.INFO, boardInfo() ));
+        webSocketServer.sendMessage(UTIL.makeCommand( CMD.PRIVATE_INFO, userInfo(user) ), user.id);
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.CHAT, UTIL.getMessage(MESSAGE.MSG_JOIN, user.id) ));
+    }
+
+    //Processing function when the game start
+    function processStart(user) {
+
+        gamePlayingFlag = true;
+        rummikub.initializeGame();
+
+        // select next turn player    
+        currentPlayer = rummikub.users[turnCount % rummikub.users.length];
+
+        for(var idx in rummikub.users) {
+            webSocketServer.sendMessage(UTIL.makeCommand( CMD.START, userInfo(rummikub.users[idx]) ), rummikub.users[idx].id);
+            webSocketServer.sendMessage(UTIL.makeCommand( CMD.PRIVATE_INFO, userInfo(rummikub.users[idx]) ), rummikub.users[idx].id);
+        }
+
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.CHAT, MESSAGE.MSG_START) );
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.CHAT, UTIL.getMessage(MESSAGE.MSG_NEXT_TURN, currentPlayer.id) ));
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.INFO, boardInfo() ));
+
+        //Timer Start
+        sandGlassTimer(true);
+
+    }
+
+    //Processing function for each turn
+    function processTurn(param, user) {
+
+        var validateResult = false;
+
+        turnCount++;
+        // select next turn player
+        currentPlayer = rummikub.users[turnCount % rummikub.users.length];
+        
+        for(var i=0; i<BOARD.HEIGHT; i++) {
+			for(var j=0; j<BOARD.WIDTH; j++) {
+				var tile = param.board[(i*BOARD.WIDTH)+j];
+
+				if(tile != null) {
+					if(tile.isOwn == true) {
+						user.use.push(tile);
+						user.removeOwnTile(tile);    
+					}
+				}
+				
+			}
+        }
+
+        if(user.use.length == 0) {
+            processPenalty(user, param.isTimeout);
+        }else {
+
+            if(rummikub.validateTile(param.board)) {
+                if(user.registerYN) {
+
+                    if(user.own.length == 0) {
+                        processWin();
+                        return;
+                    }
+
+                    validateResult = true;
+
+                }else {
+                    if(user.validateRegisterTile()) {
+                        user.registerYN = true;
+                        validateResult = true;
+                    }else {
+                        processRollback(user);
+                        processPenalty(user, param.isTimeout);
+                    }
+                }
+
+            }else {
+                processRollback(user);
+                processPenalty(user, param.isTimeout);
+            }
+
+        }
+
+        user.use = [];
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.TURN ));
+
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.CHAT, UTIL.getMessage(MESSAGE.MSG_TURN, user.id) ));
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.CHAT, UTIL.getMessage(MESSAGE.MSG_NEXT_TURN, currentPlayer.id) ));
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.INFO, boardInfo() ));
+
+        if(validateResult) {
+            lastSyncTiles = param.board;
+        }
+
+        //Timer reStart
+        sandGlassTimer(true);
+        
+    }
+
+
+
+    function processRollback(user) {
+        webSocketServer.sendMessage(UTIL.makeCommand( CMD.ROLLBACK, user.use), user.id);
+        user.own = user.own.concat(user.use);
+        processSync(lastSyncTiles);
+    }
+
+    //Processing function for penalty
+    function processPenalty(user, isTimeout) {
+
+        var numberOfPenaltyTile = isTimeout ? BOARD.PENALTY_THREE : BOARD.PENALTY_ONE;
+        var penaltyTiles = rummikub.penaltyTile(numberOfPenaltyTile);
+
+        user.own = user.own.concat(penaltyTiles);
+
+        webSocketServer.sendMessage(UTIL.makeCommand( CMD.PENALTY, penaltyTiles), user.id); 
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.CHAT, UTIL.getMessage(MESSAGE.MSG_PENALTY, user.id, numberOfPenaltyTile) ));
+    }
+
+    //Processing function exit
+    function processExit() {
+        gamePlayingFlag = false;
+		
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.CHAT, MESSAGE.MSG_EXIT));
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.INFO, boardInfo() ));
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.EXIT ));
+
+        //Timer End
+        sandGlassTimer(false);
+    }
+
+    //Processing function for winning
+    function processWin() {
+        gamePlayingFlag = false;
+		
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.CHAT, UTIL.getMessage(MESSAGE.MSG_WIN, user.id) ));
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.INFO, boardInfo() ));
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.WIN, userScoreInfo() ));
+    }
+
+    //Processing function for synving board information
+    function processSync(param) {
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.SYNC, param ));
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.INFO, boardInfo() ));
+    }
+
+    //Processing function for the chat
+    function processChat(message) {
+
+        if(message.indexOf(INLINE_CMD.HELP) == 0 ) {
+
+            processHelp(message);
+
+        }else if(message.indexOf(INLINE_CMD.CHANGE_NAME) == 0 ) {
+
+            processChangeName(message);
+			
+        }else if(message.indexOf(INLINE_CMD.RESTART) == 0 ) {
+            processRestart(message);
+
+        }else {
+            var obj = {};
+            obj.text = user.id + " : " + message;
+            obj.color = user.chatColor;
+            webSocketServer.broadcast(UTIL.makeCommand(CMD.CHAT, obj));          
+        }
+
+    }
+
+    //Processing functoin for disconnecting
+    function processDisconnect(user) {
+        console.log("websocket connection close");
+
+        //client & connect count delete        
+        rummikub.removeUser(user.id);
+        turnCount = 1;
+
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.CHAT, UTIL.getMessage(MESSAGE.MSG_DISCONNECT, user.id) ));
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.INFO, boardInfo() ));
+
+        if(gamePlayingFlag == true) {
+            webSocketServer.broadcast(UTIL.makeCommand( CMD.DISCONNECT, user.id ));
+            processExit();
+        }
+        
+    }
+
+    //Processing functoin for when help function is initiated
+    function processHelp(param){
+        webSocketServer.sendMessage(UTIL.makeCommand( CMD.CHAT, UTIL.getMessage(MESSAGE.MSG_HELP) ), user.id);
+    } 
+
+    //Processing function for when the name changing funtion is initiated
+    function processChangeName(param){
+
+        var newName = param.split(" ")[1];
+        var originName = user.id;
+
+        //change user id
+        user.id = newName;
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.CHAT, UTIL.getMessage(MESSAGE.MSG_CHANGE_NAME, originName, newName) ));
+        webSocketServer.sendMessage(UTIL.makeCommand( CMD.PRIVATE_INFO, userInfo(user) ), user.id);
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.INFO, boardInfo() ));
+    }
+
+    //Processing function for when the restarting function is initiated
+    function processRestart(param){
+        window.location.reload();
+    }
+
+    //60 second timer 
+    function sandGlassTimer(enable) {
+
+        if(timer != null) {
+            clearInterval(timer); 
+        }
+
+        if(enable) {
+            sec = BOARD.TIMER_SEC;
+            timer = setInterval(function(){
+                sandGlassTrigger();
+            }, BOARD.TIMER_INTERVAL);
+        }else {
+            sec = 0;
+        }
+
+    }
+
+    //Triggering funtion for the timer
+    function sandGlassTrigger() {
+        var param = {};
+        param.sec = sec;
+        param.currentPlayerID = currentPlayer.id;
+        webSocketServer.broadcast(UTIL.makeCommand( CMD.TIMER, param ));
+        sec--;
+    }
+
+})
